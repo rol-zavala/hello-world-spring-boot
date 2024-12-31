e629e7b5780147ce37f75d8018cf2ff4d84ad80d
rol-zavala <zrolando1015@gmail.com>
2024-12-30 20:10:19 -0600

    hello world

---
+++ src/main/java/com/devops_ex/hello_world/HelloWorldControlller.java
@@ -0,0 +1,13 @@
+package com.devops_ex.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class HelloWorldControlller {
+
+    @GetMapping()
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/devops_ex/hello_world/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops_ex.hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
